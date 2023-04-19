59ee3e48a45749ed73925c3412dc1eee1e3374fd
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2023-04-19 23:02:07 +0200

    Add config validation & rework lun config

--- DellPSPlugin.pm
+++ DellPSPlugin.pm
@@ -29,15 +29,45 @@ sub getmultiplier {
     return $mp;
 }
 
+sub validate_config {
+    my ($scfg) = @_;
+
+    # Validate mutipath config
+    my $multipath_service_state = run_command("systemctl list-units --all --state=active --type=service | grep multipath", noerr => 1, quiet => 1);
+    $multipath_service_state = $multipath_service_state == 0 ? 1 : 0; # Grep retcode equal 0 if result found
+    if ("$multipath_service_state" ne $scfg->{multipath}) {
+        print "Mismatch between plugin config and service state for multipath.\n";
+        print "Plugin config : ", $scfg->{multipath}, "\n";
+        print "Service state : ", $multipath_service_state, "\n";
+
+        return 0;
+    };
+
+    # Add check for autologin
+    return 1;
+}
+
 sub dell_connect {
     my ($scfg) = @_;
+
+    # Validate config before openning a connection
+    my $isValid = validate_config($scfg);
+    if (!$isValid) {
+        die "dell_connect: Validation of the configuration failed.\n";
+    };
+
+    # Configure Telnet client
     my $obj = new Net::Telnet(
 	Host => $scfg->{adminaddr},
+    # Uncomment to activate logs (debug only)
 	#Input_log  => "/tmp/dell.log",
 	#Output_log => "/tmp/dell.log",
     );
 
+    # Initialize session with credentials
     $obj->login($scfg->{login}, $scfg->{password});
+
+    # Configure Dell PS cli for this telnet sessions
     $obj->cmd('cli-settings events off');
     $obj->cmd('cli-settings formatoutput off');
     $obj->cmd('cli-settings confirmation off');
@@ -46,6 +76,8 @@ sub dell_connect {
     $obj->cmd('cli-settings paging off');
     $obj->cmd('cli-settings reprintBadInput off');
     $obj->cmd('stty hardwrap off');
+
+    # Return telnet session
     return $obj;
 }
 
@@ -59,11 +91,28 @@ sub dell_create_lun {
 
 sub dell_configure_lun {
     my ($scfg, $cache, $name) = @_;
+
     $cache->{telnet} = dell_connect($scfg) unless $cache->{telnet};
     my $tn = $cache->{telnet};
 
-    # unrestricted-access. Rewrite for production
-    $tn->cmd("volume select $name access create ipaddress *.*.*.* authmethod none");
+    if ((!defined($scfg->{allowedaddr}) || $scfg->{allowedaddr} eq '') &&  (!defined($scfg->{chaplogin}) || $scfg->{chaplogin} eq '')) {
+        # No allowedaddr nor chaplogin => unrestricted-access.
+        $tn->cmd(sprintf("volume select %s access create ipaddress *.*.*.* authmethod none", $name));
+    } elsif ((!defined($scfg->{allowedaddr}) || $scfg->{allowedaddr} eq '') &&  (defined($scfg->{chaplogin}) && $scfg->{chaplogin} ne '')) {
+        # Only a chaplogin given => access through auth on any ip
+        $tn->cmd(sprintf("volume select %s access create ipaddress *.*.*.* username %s authmethod chap", $name, $scfg->{chaplogin}));
+    } elsif ((defined($scfg->{allowedaddr}) && $scfg->{allowedaddr} ne '')){
+        my $usernamestr = '';
+        if (defined($scfg->{chaplogin}) && $scfg->{chaplogin} ne '') {
+            $usernamestr = "username " . $scfg->{chaplogin} . " authmethod chap ";
+        };
+        
+        my@allowedaddr = split(' ', $scfg->{allowedaddr});
+        foreach my $addr ( @allowedaddr ) {
+            $tn->cmd(sprintf("volume select %s access create ipaddress %s %s", $name, $addr, $usernamestr));
+        };
+    };
+
     # PVE itself manages access to LUNs, so that's OK.
     $tn->cmd("volume select $name multihost-access enable");
 }
@@ -157,6 +206,13 @@ sub dell_status {
     }
 }
 
+
+sub iscsi_enable {
+    my ($class, $scfg, $cache, $name) = @_;
+
+    my $target = dell_get_lun_target($scfg, $cache, $name) || die "Cannot get iscsi tagret name";
+}
+
 sub multipath_enable {
     my ($class, $scfg, $cache, $name) = @_;
 
@@ -168,18 +224,18 @@ sub multipath_enable {
 	run_command(['/usr/bin/iscsiadm', '-m', 'node', '--targetname', $target, '--portal', $scfg->{'groupaddr'} .':3260', '--rescan']);
     } else {
 	# Discover portal for new targets
-	run_command(['/usr/bin/iscsiadm', '-m', 'discovery', '--portal', $scfg->{'groupaddr'} .':3260', '--discover']);
+	run_command(['/usr/bin/iscsiadm', '-m', 'discovery','--type', 'sendtargets', '--portal', $scfg->{'groupaddr'} .':3260']);
 
 	# Login to target. Will produce warning if already logged in. But that's safe.
-	run_command(['/usr/bin/iscsiadm', '-m', 'node', '--targetname', $target, '--portal', $scfg->{'groupaddr'} .':3260', '--login']);
+	run_command(['/usr/bin/iscsiadm', '-m', 'node', '--targetname', $target, '--portal', $scfg->{'groupaddr'} .':3260', '--login'], noerr => 1);
     }
 
     sleep 1;
 
     # wait udev to settle divices
-    run_command(['/sbin/udevadm', 'settle']);
+    run_command(['/usr/bin/udevadm', 'settle']);
     #force devmap reload to connect new device.
-    run_command(['/sbin/multipath', '-r']);
+    run_command(['/usr/sbin/multipath', '-r']);
 }
 
 sub multipath_disable {
@@ -218,35 +274,51 @@ sub plugindata {
 sub properties {
     return {
         groupaddr => {
-            description => "Group address of storage (for iscsi mounts)",
-            type => 'string', format => 'pve-storage-server',
+            description => "Group IP (or DNS name) of storage for iscsi mounts",
+            type => 'string', format => 'pve-storage-portal-dns',
         },
-	adminaddr => {
-	    description => "Management IP or DNS name of storage.",
-	    type => 'string', format => 'pve-storage-server',
-	},
-	login => {
-	    description => "login",
-	    type => 'string',
-	},
-	password => {
-	    description => "password",
-	    type => 'string',
-	},
+        adminaddr => {
+            description => "Management IP (or DNS name) of storage.",
+            type => 'string', format => 'pve-storage-portal-dns',
+        },
+        login => {
+            description => "Volume admin login",
+            type => 'string',
+        },
+        password => {
+            description => "Volume admin password",
+            type => 'string',
+        },
+        allowedaddr => {
+            description => "Allowed ISCSI client IP list (space separated)",
+            type => 'string',
+        },
+        chaplogin => {
+            description => "CHAP login used in iscsi.conf",
+            type => 'string',
+        },
+        multipath => {
+            description => "Volume admin password",
+            type => 'boolean',
+        },
+        
     };
 }
 
 sub options {
     return {
-	groupaddr => { fixed => 1 },
-	pool  => { fixed => 1 },
-	login => { fixed => 1 },
-	password => { fixed => 1 },
-	adminaddr => { fixed => 1 },
+        groupaddr => { fixed => 1 },
+        pool  => { fixed => 1 },
+        login => { fixed => 1 },
+        password => { fixed => 1 },
+        adminaddr => { fixed => 1 },
+        chaplogin => { optional => 1 },
+        allowedaddr => { optional => 1 },
+        multipath => { fixed => 1 },
         nodes   => { optional => 1 },
-	disable => { optional => 1 },
-	content => { optional => 1 },
-	shared  => { optional => 1 },
+        disable => { optional => 1 },
+        content => { optional => 1 },
+        shared  => { optional => 1 },
     }
 }
 
--- test_DellPSPlugin.pl
+++ test_DellPSPlugin.pl
@@ -1,11 +1,48 @@
 use strict;
 use warnings;
 use Module::Load;
-use PVE::CLIHandler;
+use Data::Dumper;
 
 load './DellPSPlugin.pm';
+use PVE::Storage::Custom::DellPSPlugin;
 
 print "Test script for DellPSPlugin\n";
 
-PVE::CLIHandler::setup_environment();
-print "CLI initialized\n";
+print "Configure plugin\n";
+my $scfg = {
+    login => "pve",
+    password => "3lectroPV3",
+    adminaddr => "172.16.50.42",
+    groupaddr => "172.16.7.28",
+    chaplogin => "pve-electrolab",
+    #allowedaddr => "172.16.7.42 172.16.7.43",
+    multipath => "0",
+    pool => "default",
+    content => "images",
+    shared => "1",
+};
+print Dumper($scfg);
+
+#################################
+#                               #
+#   Test primitives functions   #
+#                               #
+#################################
+print "Initialize connection to the Dell PS\n";
+my $cache;
+$cache->{telnet} = PVE::Storage::Custom::DellPSPlugin::dell_connect($scfg);
+
+print "Get Dell PS storage status\n";
+my @status = @{PVE::Storage::Custom::DellPSPlugin::dell_status($scfg, $cache)};
+
+print "Status:\n";
+printf("\t- Total space: %d GB\n", int($status[0]) / PVE::Storage::Custom::DellPSPlugin::getmultiplier('GB'));
+printf("\t- Free space: %d GB\n", int($status[1]) / PVE::Storage::Custom::DellPSPlugin::getmultiplier('GB'));
+printf("\t- Used space: %d GB\n", int($status[2]) / PVE::Storage::Custom::DellPSPlugin::getmultiplier('GB'));
+printf("\t- Active : %s\n", $status[3]);
+
+print "Create the Volume (LUN) : vm-20-disk-1\n";
+#PVE::Storage::Custom::DellPSPlugin::dell_create_lun($scfg, $cache, 'vm-20-disk-1', '1GB');
+
+print "Allow PVE nodes to access Volumes (LUN)\n";
+PVE::Storage::Custom::DellPSPlugin::dell_configure_lun($scfg, $cache, 'vm-20-disk-1');
